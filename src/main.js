2a48ab5f502e9049f70a59421de007a2600b9860
DanielPincu <ghepin01@easv365.dk>
2024-08-18 18:55:22 +0200

    Revert "Vuetify added"
    This reverts commit ac81f270fc5e74e2eb4f8bc5e1aaf9ca96e67fd4.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import './style.css'
+import App from './App.vue'
 
-import 'vuetify/styles' // Import Vuetify's styles
-import { createVuetify } from 'vuetify' // Import Vuetify
-import * as components from 'vuetify/components' // Import all Vuetify components
-import * as directives from 'vuetify/directives' // Import all Vuetify directives
-
-// Create a Vuetify instance with the components and directives
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
-const app = createApp(App)
-
-// Use Vuetify instance in the app
-app.use(vuetify)
-
-app.mount('#app')
+createApp(App).mount('#app')
