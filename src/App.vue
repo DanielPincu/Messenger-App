1c29a34dc23d25d7a76a5c664b0d02ecca71e706
DanielPincu <ghepin01@easv365.dk>
2024-09-01 10:50:29 +0200

    glitch updated

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="w-full h-screen bg-blue-100">
+  <div id="app" class="w-full md:h-screen bg-blue-100">
     <div v-if="!username" class="flex items-center justify-center h-full">
       <Login @login="handleLogin" />
     </div>
